c47f69e61fd57722774f679330629bd4c76b21fe
Riton <ritontian@outlook.com>
2022-07-21 02:04:11 +0800

    figure out why shell and insert slower

--- README.md
+++ README.md
@@ -142,3 +142,121 @@ compare count: 6820966545
 cost time: 4607.58ms
 *******************
 ```
+尝试将比较操作放入for条件内后，插入和希尔排序比较次数大幅下降，速度变快：
+
+- 插入排序
+
+```c++
+void insert_sort(double *v) {
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        for (int j = i; j > 0 && less(v[j], v[j - 1]); j--) {
+            exchange(v, j, j - 1);
+        }
+
+// slow way
+//        for (int j = i; j > 0; j--) {
+//            if (less(v[j], v[j - 1])) {
+//                exchange(v, j, j - 1);
+//            }
+//        }
+    }
+}
+```
+
+- 希尔排序
+
+```c++
+void shell_sort(double *v) {
+    int h = 1;
+    while (h < (ARRAY_SIZE / 3))
+        h = 3 * h + 1;
+
+    while (h >= 1) {
+        for (int i = h; i < ARRAY_SIZE; i++) {
+            for (int j = i; j >= h && less(v[j], v[j - h]); j -= h) {
+                exchange(v, j, j - h);
+            }
+// slow way
+//            for (int j = i; j >= h; j -= h) {
+//                if (less(v[j], v[j - h]))
+//                    exchange(v, j, j - h);
+//            }
+        }
+
+        h = h / 3;
+    }
+}
+```
+输出如下：
+```
+**** select sort ****
+----ordered vector----
+exchange count: 100000
+compare count: 5000050000
+cost time: 2.26638s
+----10 random vector----
+average exchange count: 100000
+average compare count: 5.00005e+09
+cost time: 22.97s
+----reversed vector----
+exchange count: 100000
+compare count: 5000050000
+cost time: 2.27603s
+*******************
+**** insert sort ****
+----ordered vector----
+exchange count: 0
+compare count: 99999
+cost time: 7.3441e-05s
+----10 random vector----
+average exchange count: 2.49981e+09
+average compare count: 2.49991e+09
+cost time: 11.666s
+----reversed vector----
+exchange count: 4999950000
+compare count: 4999950000
+cost time: 2.38609s
+*******************
+**** shell sort ****
+----ordered vector----
+exchange count: 0
+compare count: 967146
+cost time: 0.000878631s
+----10 random vector----
+average exchange count: 2.97707e+06
+average compare count: 3.90292e+06
+cost time: 0.150304s
+----reversed vector----
+exchange count: 619654
+compare count: 1533494
+cost time: 0.00116666s
+*******************
+```
+最后发现原因在于，for的条件隐含着break的意义，例如，两种插入排序展开后如下：
+```c++
+    //fast
+    for (int i = 0; i < size; ++i) {
+        int j = i;
+        while(true) {
+            if (!(j > 0)) { break; } //middle condition
+            if (!less(v1[j], v1[j - 1])) { break; } //middle condition
+            exchange(v1, j, j - 1);
+            j--;
+        }
+    }
+
+    //slower
+    for (int i = 0; i < size; ++i) {
+       int j = i;
+       while(true) {
+          if (!(j > 0)) { break; } //middle condition
+          if (!less(v2[j], v2[j - 1])) {
+              //not break
+          } else {
+              exchange(v2, j, j - 1);
+          }
+          j--;
+       }
+    }
+
+```
--- basic/origin_array_sort.cpp
+++ basic/origin_array_sort.cpp
@@ -6,27 +6,31 @@
 #include <vector>
 #include <cassert>
 #include <ctime>
+#include <chrono>
 
 
-unsigned int ARRAY_SIZE = 100000;
+const unsigned int ARRAY_SIZE = 100000;
+const unsigned int TEST_TIME = 10;
+long int exchange_count = 0;
+long int compare_count = 0;
 
-std::vector<int> build_sorted_vector() {
-    std::vector<int> v = {};
+std::vector<double> build_sorted_vector() {
+    std::vector<double> v = {};
     for (int i = 0; i < ARRAY_SIZE; ++i) {
         v.push_back(i);
     }
     return v;
 }
 
-std::vector<int> build_random_vector() {
+std::vector<double> build_random_vector() {
     auto v = build_sorted_vector();
     auto rng = std::mt19937(std::random_device()());
     std::shuffle(v.begin(), v.end(), rng);
     return v;
 }
 
-int* convert_vector_to_array(std::vector<int> &v) {
-    auto arr = new int[ARRAY_SIZE];
+double* convert_vector_to_array(std::vector<double> &v) {
+    auto arr = new double[ARRAY_SIZE];
     auto index = 0;
     for (auto i: v) {
         arr[index] = i;
@@ -35,19 +39,14 @@ int* convert_vector_to_array(std::vector<int> &v) {
     return arr;
 }
 
-#define quick_return(v) {/*if ((v).empty() || (v).size() == 1) return;*/}
-
-long int exchange_count = 0;
-long int compare_count = 0;
-
-void exchange(int *v, unsigned int i, unsigned int j) {
+void exchange(double *v, unsigned int i, unsigned int j) {
     exchange_count++;
     auto temp = v[i];
     v[i] = v[j];
     v[j] = temp;
 }
 
-bool less(int m, int n) {
+bool less(double m, double n) {
     compare_count++;
     return m < n;
 }
@@ -61,8 +60,8 @@ void show_count() {
     std::cout << "compare count: " << compare_count << std::endl;
 }
 
-std::vector<int> convert_array_to_vector(int *arr) {
-    auto v = std::vector<int>();
+std::vector<double> convert_array_to_vector(double *arr) {
+    auto v = std::vector<double>();
 
     for (int i = 0; i < ARRAY_SIZE; ++i) {
         v.push_back(arr[i]);
@@ -71,7 +70,7 @@ std::vector<int> convert_array_to_vector(int *arr) {
     return v;
 }
 
-void check_sorted(int *arr) {
+void check_sorted(double *arr) {
     auto v = convert_array_to_vector(arr);
     assert(std::is_sorted(v.begin(), v.end()));
 }
@@ -81,15 +80,13 @@ void show_reset_count() {
     reset_count();
 }
 
-void check_show_reset_count(int *p) {
+void check_show_reset_count(double *p) {
     check_sorted(p);
     show_reset_count();
 }
 
 //选择排序
-void select_sort(int *v) {
-    quick_return(v);
-
+void select_sort(double *v) {
     auto index_to_put = 0;
     while (index_to_put < ARRAY_SIZE) {
         auto min_value_index = index_to_put;
@@ -103,10 +100,8 @@ void select_sort(int *v) {
     }
 }
 
-//插入排序
-void insert_sort(int *v) {
-    quick_return(v);
-
+//插入排序 slow
+void insert_sort_slow(double *v) {
     for (int i = 0; i < ARRAY_SIZE; ++i) {
         for (int j = i; j > 0; j--) {
             if (less(v[j], v[j - 1])) {
@@ -116,56 +111,114 @@ void insert_sort(int *v) {
     }
 }
 
-//希尔排序
-void shell_sort(int *v) {
-    quick_return(v);
+//插入排序
+void insert_sort(double *v) {
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        for (int j = i; j > 0 && less(v[j], v[j - 1]); j--) {
+            exchange(v, j, j - 1);
+        }
+    }
+}
+//  init                2,| 9, 7, 3, 8, 4, 5, 6, 0, 1
+//  compare 9 and 2     2, 9,| 7, 3, 8, 4, 5, 6, 0, 1
+//  compare 7 and 9     2, 7, 9,| 3, 8, 4, 5, 6, 0, 1
+//  compare 7 and 2     2, 7, 9,| 3, 8, 4, 5, 6, 0, 1
+//  compare 3 and 9     2, 7, 3, 9,| 8, 4, 5, 6, 0, 1
+//  compare 3 and 7     2, 3, 7, 9,| 8, 4, 5, 6, 0, 1
+//  compare 3 and 2     2, 3, 7, 9,| 8, 4, 5, 6, 0, 1
+//  compare 8 and 9     2, 3, 7, 8, 9,| 4, 5, 6, 0, 1
+//  compare 8 and 7     2, 3, 7, 8, 9,| 4, 5, 6, 0, 1
+//* compare 4 and 9     2, 3, 7, 8, 9,| 4, 5, 6, 0, 1
+void insert_sort_test() {
+    const int size = 10;
+    double v1[size] = {2, 9, 7, 3, 8, 4, 5, 6, 0, 1};
 
+    for (int i = 0; i < size; ++i) {
+        int j = i;
+        while(true) {
+            if (!(j > 0)) { break; } //middle condition
+            if (!less(v1[j], v1[j - 1])) { break; } //middle condition
+            exchange(v1, j, j - 1);
+            j--;
+        }
+    }
+
+    show_reset_count();
+
+    double v2[size] = {2, 9, 7, 3, 8, 4, 5, 6, 0, 1};
+    for (int i = 0; i < size; ++i) {
+        int j = i;
+        while(true) {
+            if (!(j > 0)) { break; } //middle condition
+            if (!less(v2[j], v2[j - 1])) {
+
+            } else {
+                exchange(v2, j, j - 1);
+            }
+            j--;
+        }
+    }
+    show_reset_count();
+}
+
+//希尔排序
+void shell_sort(double *v) {
     int h = 1;
     while (h < (ARRAY_SIZE / 3))
         h = 3 * h + 1;
 
     while (h >= 1) {
         for (int i = h; i < ARRAY_SIZE; i++) {
-            for (int j = i; j >= h; j -= h) {
-                if (less(v[j], v[j - h]))
-                    exchange(v, j, j - h);
+            for (int j = i; j >= h && less(v[j], v[j - h]); j -= h) {
+                exchange(v, j, j - h);
             }
+// slow way
+//            for (int j = i; j >= h; j -= h) {
+//                if (less(v[j], v[j - h]))
+//                    exchange(v, j, j - h);
+//            }
         }
 
         h = h / 3;
     }
 }
 
-unsigned int TEST_TIME = 10;
 
-void show_cost(double &cost) {
-    std::cout << "cost time: " << cost << "ms" << std::endl;
-    cost = 0;
+void show_cost(std::chrono::duration<double> &cost) {
+    std::cout << "cost time: " << cost.count() << "s" << std::endl;
+    cost = cost - cost;
 }
 
-void test_group(void (*f)(int *p), const std::string &name) {
+void test_group(void (*f)(double *p), const std::string &name, bool single_random = false) {
+    using namespace std::chrono;
     std::cout << "**** " << name << " sort ****" << std::endl;
-    std::cout << "----ordered vector----" << std::endl;
-    auto v = build_sorted_vector();
-    auto arr = convert_vector_to_array(v);
+    std::vector<double> v;
+    double *arr;
+    time_point<steady_clock> start, end;
+    duration<double> cost{};
+    if (!single_random) {
+        std::cout << "----ordered vector----" << std::endl;
+        v = build_sorted_vector();
+        arr = convert_vector_to_array(v);
 
-    auto start = clock();
-    f(arr);
-    auto end = clock();
-    auto cost = (double(end - start) / CLOCKS_PER_SEC) * 1000;
+        start = steady_clock::now();
+        f(arr);
+        end = steady_clock::now();
+         cost = duration_cast<duration<double>>(end - start);
 
-    check_show_reset_count(arr);
-    show_cost(cost);
+        check_show_reset_count(arr);
+        show_cost(cost);
+    }
 
     std::cout << "----" << TEST_TIME << " random vector----" << std::endl;
     for (int i = 0; i < TEST_TIME; ++i) {
         v = build_random_vector();
         arr = convert_vector_to_array(v);
 
-        start = clock();
+        start = steady_clock::now();
         f(arr);
-        end = clock();
-        cost += (double(end - start) / CLOCKS_PER_SEC) * 1000;
+        end = steady_clock::now();
+        cost += duration_cast<duration<double>>(end - start);
 
         check_sorted(arr);
     }
@@ -176,18 +229,20 @@ void test_group(void (*f)(int *p), const std::string &name) {
     reset_count();
 
 
-    std::cout << "----reversed vector----" << std::endl;
-    v = build_sorted_vector();
-    std::reverse(v.begin(), v.end());
-    arr = convert_vector_to_array(v);
+    if (!single_random) {
+        std::cout << "----reversed vector----" << std::endl;
+        v = build_sorted_vector();
+        std::reverse(v.begin(), v.end());
+        arr = convert_vector_to_array(v);
 
-    start = clock();
-    f(arr);
-    end = clock();
-    cost = (double(end - start) / CLOCKS_PER_SEC) * 1000;
+        start = steady_clock::now();
+        f(arr);
+        end = steady_clock::now();
+        cost = duration_cast<duration<double>>(end - start);
 
-    check_show_reset_count(arr);
-    show_cost(cost);
+        check_show_reset_count(arr);
+        show_cost(cost);
+    }
     std::cout << "*******************" << std::endl;
 }
 
@@ -196,4 +251,7 @@ int main() {
     test_group(insert_sort, std::string("insert"));
     test_group(shell_sort, std::string("shell"));
 
+//    test_group(insert_sort, std::string("fast"), true);
+//    test_group(insert_sort_slow, std::string("slow"), true);
+//    insert_sort_test();
 }
